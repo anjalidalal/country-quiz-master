e0ea85e56bd5fb0aad940b4f3ab34360bddc45e4
anjalidalal <anjalimaria2002@gmail.com>
2022-03-19 17:16:27 +0530

    Responsive webpage(Mobile view and laptop view)

--- space.js
+++ space.js
@@ -4,77 +4,89 @@ const btn3 = document.getElementById("three");
 const btn4 = document.getElementById("four");
 const next = document.querySelector(".next");
 const reults = document.querySelector(".main2");
-const main1 = document.querySelector(".main")
+const main1 = document.querySelector(".main");
+
+let isOptionSlected = false;
+
+let vectorRight = document.createElement("img");
+let wrong = document.createElement("img");
+
+vectorRight.setAttribute("class", "right");
+wrong.setAttribute("class", "wrong");
+
+
 
 function buttonOne() {
-    // btn1.setAttribute("class", "wrong");
-    // btn2.setAttribute("class", "right")
+    if (isOptionSlected) {
+        return;
+    }
+    isOptionSlected = true;
 
-    let vectorRight = document.createElement("img");
     vectorRight.src = "icons8-done-30.png";
 
     btn2.appendChild(vectorRight);
     btn2.style.backgroundColor = "#60BF88";
 
-    const wrong = document.createElement("img");
+ 
     wrong.src = "icons8-multiply-30.png";
 
     btn1.appendChild(wrong);
     btn1.style.backgroundColor = "#EA8282";
 
-    next.style.display = "unset"
- 
+    next.style.display = "unset";
 }
 
 function buttonTwo() {
-    // btn2.setAttribute("class", "right");
+    if (isOptionSlected) {
+        return;
+    }
+    isOptionSlected = true;
 
-    let vectorRight = document.createElement("img");
     vectorRight.src = "icons8-done-30.png";
 
     btn2.appendChild(vectorRight);
     btn2.style.backgroundColor = "#60BF88";
+    
 
-    next.style.display = "unset"
+    next.style.display = "unset";
 }
 
 function buttonThree() {
-    // btn2.setAttribute("class", "right");
-    // btn3.setAttribute("class", "wrong");
-    
-    let vectorRight = document.createElement("img");
+    if (isOptionSlected) {
+        return;
+    }
+    isOptionSlected = true;
+
     vectorRight.src = "icons8-done-30.png";
 
     btn2.appendChild(vectorRight);
     btn2.style.backgroundColor = "#60BF88";
-    
-    const wrong = document.createElement("img");
+
     wrong.src = "icons8-multiply-30.png";
 
     btn3.appendChild(wrong);
     btn3.style.backgroundColor = "#EA8282";
 
-    next.style.display = "unset"
+    next.style.display = "unset";
 }
 
 function buttonFour() {
-    // btn2.setAttribute("class", "right");
-    // btn4.setAttribute("class", "wrong")
-   
-    let vectorRight = document.createElement("img");
+    if (isOptionSlected) {
+        return;
+    }
+    isOptionSlected = true;
+
     vectorRight.src = "icons8-done-30.png";
 
     btn2.appendChild(vectorRight);
     btn2.style.backgroundColor = "#60BF88";
-    
-    const wrong = document.createElement("img");
+
     wrong.src = "icons8-multiply-30.png";
 
     btn4.appendChild(wrong);
     btn4.style.backgroundColor = "#EA8282";
 
-    next.style.display = "unset"
-
+    next.style.display = "unset";
 }
 
 const nextBtn = document.getElementById("btn");
@@ -82,40 +94,124 @@ const heading = document.getElementById("heading");
 const flag = document.querySelector(".icon");
 // const tryAgainBtn = document.querySelector(".btn2");
 
-
 function goToNextPage() {
-   const icon = document.createElement("img");
-   icon.src = "icons8-finland-48.png";
-   icon.setAttribute("class", "flag")
+    const options = document.querySelectorAll(".option");
+    options.forEach(function (option) {
+        option.style.backgroundColor = "";
+    });
+    const icon = document.createElement("img");
+    icon.src = "icons8-finland-48.png";
+    icon.setAttribute("class", "flag");
 
-   flag.appendChild(icon);
+    flag.appendChild(icon);
 
-   heading.textContent = "Which country does this flag belong to?"
-   heading.style.marginTop = "49px"
-   heading.style.paddingTop = "13px";
-   heading.setAttribute("class", "head")
+    heading.textContent = "Which country does this flag belong to?";
+    heading.style.marginTop = "49px";
+    heading.style.paddingTop = "13px";
+    heading.setAttribute("class", "head");
+    const array = ["A Vitenam", "B Finland", "C Sweden", "D Austria"];
 
-   btn2.textContent = "Finland";
+    options.forEach(function (option, index) {
+        option.textContent = array[index];
+    });
 
-   next.style.display = "none"
+    next.style.display = "none";
+    let isOptionSlected = false;
 
-   nextBtn.addEventListener("click", reultsPage)
-    
+    btn1.addEventListener("click", function () {
+
+        if (isOptionSlected) {
+            return;
+        }
+        isOptionSlected = true;
+
+        vectorRight.src = "icons8-done-30.png";
+
+        btn2.appendChild(vectorRight);
+        btn2.style.backgroundColor = "#60BF88";
+
+        wrong.src = "icons8-multiply-30.png";
+
+        btn1.appendChild(wrong);
+        btn1.style.backgroundColor = "#EA8282";
+
+        next.style.display = "unset";
+    });
+
+    btn2.addEventListener("click", function () {
+
+        if (isOptionSlected) {
+            return;
+        }
+        isOptionSlected = true;
+
+        vectorRight.src = "icons8-done-30.png";
+
+        btn2.appendChild(vectorRight);
+        btn2.style.backgroundColor = "#60BF88";
+
+        next.style.display = "unset";
+    });
+
+    btn3.addEventListener("click", function () {
+
+        if (isOptionSlected) {
+            return;
+        }
+        isOptionSlected = true;
+
+        vectorRight.src = "icons8-done-30.png";
+
+        btn2.appendChild(vectorRight);
+        btn2.style.backgroundColor = "#60BF88";
+
+        wrong.src = "icons8-multiply-30.png";
+
+        btn3.appendChild(wrong);
+        btn3.style.backgroundColor = "#EA8282";
+
+        next.style.display = "unset";
+    });
+
+    btn4.addEventListener("click", function () {
+
+        if (isOptionSlected) {
+            return;
+        }
+        isOptionSlected = true;
+
+        vectorRight.src = "icons8-done-30.png";
+
+        btn2.appendChild(vectorRight);
+        btn2.style.backgroundColor = "#60BF88";
+
+        wrong.src = "icons8-multiply-30.png";
+
+        btn4.appendChild(wrong);
+        btn4.style.backgroundColor = "#EA8282";
+
+        next.style.display = "unset";
+    });
+
+    nextBtn.addEventListener("click", reultsPage);
 }
 
-nextBtn.addEventListener("click", goToNextPage)
+nextBtn.addEventListener("click", goToNextPage);
 
 function reultsPage() {
-  reults.style.display = "unset";
+    reults.style.display = "unset";
 
-  main1.style.display = "none";
-
+    main1.style.display = "none";
 }
 
-// function tryAgain() {
-//     main1.style.display = "unset";
 
-//     reults.style.display = "none";
-  
-// }
-// tryAgainBtn.addEventListener("click", tryAgain)
+const tryAgainBtn = document.querySelector(".btn2")
+function tryAgain() {
+    
+    reults.style.display = "none";
+
+    main1.style.display = "unset";
+    main1.style.margin = "auto"
+
+}
+tryAgainBtn.addEventListener("click", tryAgain)
